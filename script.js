6e566c4353eedae44d8dfd11da1b2aafa1e137d4
kissilaam <kissila.am@gmail.com>
2023-03-23 11:47:56 -0300

    added main char bios

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ let characterIndex = 0
 
 function getImage() {
 	let charImg = document.getElementsByClassName('character-img')
+
 	for (let i = 0; i < charImg.length; i++) {
 		charImg[i].style.display = 'none'
 	}
 	imgIndex++
 	if (imgIndex > charImg.length) imgIndex = 1
 	charImg[imgIndex - 1].style.display = 'block'
-	setTimeout(getImage, 1500)
+	setTimeout(getImage, 2000)
 }
